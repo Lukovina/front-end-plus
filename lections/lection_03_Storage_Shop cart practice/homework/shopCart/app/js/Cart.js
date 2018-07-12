6c3b07ae457e504ef4adae663b7a375de2d94fe4
Volodymyr Lukovyna <volodymyrlukovyna@MacBook-Pro-Volodymyr.local>
2018-07-13 00:55:40 +0300

    few fixex of cart

--- lections/lection_03_Storage_Shop cart practice/homework/shopCart/app/js/Cart.js
+++ lections/lection_03_Storage_Shop cart practice/homework/shopCart/app/js/Cart.js
@@ -1,17 +1,43 @@
 let data = require("./data.js");
+const cart = document.querySelector(".cart"),
+      goodsTable = document.querySelector(".goodsTable"),
+      cartButton = document.querySelector(".cartButton"),
+      closer = document.querySelector(".close"); 
 
-const cartGoods = new Set();
+cartButton.addEventListener("click", cartRender);
+closer.addEventListener("click", closeCart);
+console.dir(closer)
 
+const cartGoods = [];
 function fillCart(ident) {   
     for(let item of data.initialCondition()) {
         for(let key in item) {
             if(item[key] == ident) {
-                cartGoods.add(item)
+                cartGoods.push(item)
             }
         }
     }
 
-    localStorage.setItem("goods" , JSON.stringify([...cartGoods]))
+    localStorage.setItem("goods" , JSON.stringify(cartGoods))
+}
+
+function cartRender () {
+    cart.classList.add("show")
+    let goodsList = JSON.parse(localStorage.getItem("goods")) 
+    goodsTable.innerHTML += `
+            ${goodsList.map(good => 
+                `<tr>
+                    <td>${good.title}</td>`
+            ).join("")}
+            </tr>
+        </table>
+    `              
+}
+
+
+function closeCart(){
+    console.log("ss")
+    cart.classList.remove("show")
 }
 
 module.exports = { 
