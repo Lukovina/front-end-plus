e1e851809d51101db79796bb5c492e7a15088d8b
Volodymyr Lukovyna <volodymyrlukovyna@MacBook-Pro-Volodymyr.local>
2018-07-14 20:50:43 +0300

    shopCart practice

--- lections/lection_03_Storage_Shop cart practice/homework/shopCart/app/js/Cart.js
+++ lections/lection_03_Storage_Shop cart practice/homework/shopCart/app/js/Cart.js
@@ -1,45 +1,61 @@
-let data = require("./data.js");
+let good = require('./good.js');
+
 const cart = document.querySelector(".cart"),
       goodsTable = document.querySelector(".goodsTable"),
       cartButton = document.querySelector(".cartButton"),
+      total = document.querySelector(".totalSum"),
       closer = document.querySelector(".close"); 
+
+
+let goodsList = []
 
 cartButton.addEventListener("click", cartRender);
 closer.addEventListener("click", closeCart);
-console.dir(closer)
 
-const cartGoods = [];
-function fillCart(ident) {   
-    for(let item of data.initialCondition()) {
-        for(let key in item) {
-            if(item[key] == ident) {
-                cartGoods.push(item)
-            }
-        }
-    }
-
-    localStorage.setItem("goods" , JSON.stringify(cartGoods))
-}
 
 function cartRender () {
-    cart.classList.add("show")
-    let goodsList = JSON.parse(localStorage.getItem("goods")) 
-    goodsTable.innerHTML += `
-            ${goodsList.map(good => 
-                `<tr>
-                    <td>${good.title}</td>`
-            ).join("")}
-            </tr>
-        </table>
-    `              
+    cart.classList.add("show");
+    goodsList = JSON.parse(localStorage.getItem("goods")) ;
+
+    goodsTable.innerHTML = ""
+
+    if(!goodsList) {
+        goodsTable.innerHTML = "Корзина пуста"
+        return
+    }
+    goodsList.forEach(good => {
+       goodsTable.innerHTML+= 
+       `<tr>
+            <td colspan="3">${good.title}</td>
+            <td>${good.price}$/кг</td>
+            <td><input type="number" value="${good.quantity}"></td>
+            <td><div class="close removeGood" id="${good.id}"></div></td>
+            <td>${good.cartPrice}$</td>
+        </tr>`
+    })
+    total.innerHTML = `total Sum: ${totalSum(goodsList)} $`
+
+    let removeGood = document.querySelectorAll(`.removeGood`);   
+        removeGood.forEach(item=>item.addEventListener("click", function(){removeGoodFromCart(item.id)}))            
 }
 
+function totalSum(list){
+    return list.map(key=>key.cartPrice).reduce((prev,item)=>prev+item,0)
+   
+}
 
 function closeCart(){
-    console.log("ss")
     cart.classList.remove("show")
 }
 
+function removeGoodFromCart(ident) {
+   let localStorageModer = []
+   goodsList.forEach(item=>{if(item.id!=ident){localStorageModer.push(item)}})
+   good.fillLocalStorage(localStorageModer)
+
+  }
+
 module.exports = { 
-    fillCart
+    cartRender,
+    removeGoodFromCart
 };
--- lections/lection_03_Storage_Shop cart practice/homework/shopCart/app/js/goodsList.js
+++ lections/lection_03_Storage_Shop cart practice/homework/shopCart/app/js/goodsList.js
@@ -4,13 +4,10 @@ let cart = require("./cart.js")
 
 dataService.load("GET" , "http://localhost:3780/goods")
     .then(val => {val.data.list.forEach(el => good.renderGood(el))})
-    .then(() => addButtons(".good button", cart.fillCart))
+    .then(() => good.addButtons())
 
-function addButtons(selector, func, params) {
-    let addBtns = document.querySelectorAll(selector);
-    addBtns.forEach(elem=> elem.addEventListener("click", () => func(elem.id)))
-}
+
     
 module.exports = { 
-    addButtons
+
 };
