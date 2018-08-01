9aa2acfca4674ba814a1ae30857024b10900b55e
Volodymyr Lukovyna <volodymyrlukovyna@MacBook-Pro-Volodymyr.local>
2018-08-01 23:19:24 +0300

    finish rename Img homework

--- lections/lection_05_Gulp_npm_scripts/homework/numbers/main.js
+++ lections/lection_05_Gulp_npm_scripts/homework/numbers/main.js
@@ -1,7 +1,8 @@
 let fs = require('fs');
 let path = './lections/lection_05_Gulp_npm_scripts/homework/numbers';
-let name = "image_FR_01.png";
-let envs = process.env
+let name = "image_FR_";
+let envs = JSON.parse(process.env.env);
+let i = 0;
 console.log(envs)
 fs.readdir(path+'/images', function(err, items){
     if (err){
@@ -10,4 +11,13 @@ fs.readdir(path+'/images', function(err, items){
     }
 
     console.log(items)
-});
+    
+    items.forEach(item=>{
+        fs.rename(path + '/images/' +item, path + '/images/'+ name + envs[i] +`.png`, (err) => {
+           
+            if (err) throw err;
+            console.log('Rename complete!');            
+        })
+        i++
+    })
+})
